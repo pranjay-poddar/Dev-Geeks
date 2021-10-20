534d06f69eddfb9b501bd025cff988530526a2be
vedikamujgule <vedikamujgule@gmail.com>
2021-10-20 19:03:23 +0530

    [fix] todo conflict issues

--- React-JS Projects/covid-tracker/src/components/todoComponent/TotalCompleteItems.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-const TotalCompleteItems = (state) => {
-	console.log("items lenght", state)
-	return <h4 className='mt-3'>Total Complete Items: {state.length}</h4>;
-};
-
-export default TotalCompleteItems;
