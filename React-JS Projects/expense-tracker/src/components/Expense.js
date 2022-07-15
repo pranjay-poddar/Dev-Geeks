dc4515fd0c360c1efb871300bbd4326c93522cf8
Akanksha <akankshamitsg@gmail.com>
2022-07-15 22:54:27 +0530

    created zomato clone

--- React-JS Projects/expense-tracker/src/components/Expense.js
+++
@@ -1,8 +0,0 @@
-const Expense= ({data,onDelete})=>{
-    return(
-    <li key={data.id} className={data.amount<0 ? "minus": "plus"}>{data.text}<span>Rs. {Math.abs(data.amount)}</span><button className="delete-btn" onClick={()=> onDelete(data.id)}>x</button>
-          </li>
-    )
-}
-
-export default Expense;
