e77f83b19960a49acc001333a995af9f26978699
Srinjoy Pati <srinjoypati2595@gmail.com>
2022-08-02 20:42:15 +0530

    added selfie capture

---
+++ ML Projects/Selfi-Capture/README.md
@@ -0,0 +1,22 @@
+# Selfi-Capture
+
+## Introduction  
+This project , opens up your webcam , where on pressing space bar key it captures your selfi . The on pressing escape key it terminates the webcam.
+
+## Tech stack 
+
+The project is made using python.
+
+
+## Demonstration video - here is a view of the project :
+
+
+https://user-images.githubusercontent.com/91176055/161805234-4be7cc3d-1ac4-4a85-bfa2-bd7547b95c35.mp4
+
+## Install dependencies
+- install Python 3.8.3 or above
+- Further install these packages using terminal:
+
+```bash
+  pip install opencv-contrib-python 
+```
---
+++ ML Projects/Selfi-Capture/main.py
@@ -0,0 +1,30 @@
+import cv2
+
+cam = cv2.VideoCapture(0)
+
+cv2.namedWindow("test")
+
+img_counter = 0
+
+while True:
+    ret, frame = cam.read()
+    if not ret:
+        print("failed to grab frame")
+        break
+    cv2.imshow("test", frame)
+
+    k = cv2.waitKey(1)
+    if k%256 == 27:
+        # ESC pressed
+        print("Escape hit, closing...")
+        break
+    elif k%256 == 32:
+        # SPACE pressed
+        img_name = "opencv_frame_{}.png".format(img_counter)
+        cv2.imwrite(img_name, frame)
+        print("{} written!".format(img_name))
+        img_counter += 1
+
+cam.release()
+
+cv2.destroyAllWindows()
