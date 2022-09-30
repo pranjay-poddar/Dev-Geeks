57858a36f59fc16f3f8336633d36ee6af79677f6
Gurjeet Singh Virdee <73753957+gurjeetsinghvirdee@users.noreply.github.com>
2022-10-01 03:37:52 +0530

    editor added

---
+++ React-JS Projects/Markdown IDE/README.md
@@ -0,0 +1,8 @@
+## Markdown IDE
+It's a markdown editor so you can write the code in .md format then it will show you the results.
+
+### Tech Stack
+- React JS
+
+## Video Demonstration
+
---
+++ React-JS Projects/Markdown IDE/src/App.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './App.css';
+import Remarkable from 'remarkable';    
+
+class MarkdownEditor extends React.Component {
+    constructor(props) {
+        super(props);
+        this.handleChange = this.handleChange.bind(this);
+        this.state = { 
+            value: 'Type some *markdown* here!'
+        };
+    }
+
+
+    handleChange(e) {
+        this.setState({ 
+            value: e.target.value
+        });
+    }
+
+    getRawMarkup() {
+        const md = new Remarkable();
+        return {__html: md.render(this.state.value)};
+    }
+
+    render() {
+        return (
+            <div className="container">
+                <div className="input">
+                    <h3>Input</h3>
+                <textarea
+                    className="input-text"
+                    onChange={this.handleChange}
+                    defaultValue={this.state.value}
+                />
+                </div>
+                <div className="output">
+                    <h3>Markdown</h3>
+                <div 
+                    dangerouslySetInnerHTML={this.getRawMarkup()}
+                    className="output-text"
+                >
+                </div>
+                </div>                
+            </div>
+        )
+    }
+}
+
+// ========================================
+
+ReactDOM.render(
+    <MarkdownEditor />,
+    document.getElementById('root')
+);
