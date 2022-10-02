ff70af85df80ddebf0dc9b9f146b64c739ef721c
ayushkedia05 <kediaayush26@gmail.com>
2022-10-02 18:10:25 +0530

    dice game

--- dashboard for a classroom_manager/src/Component/rendershell.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import Shell from "./appshelll/appshell";
-const Rendershell=()=>{
-  return (
-   <Shell></Shell>
-  )
-};
-
-export default Rendershell; 
--- dashboard for a classroom_manager/src/Component/store/index.js
+++
@@ -1,7 +0,0 @@
-    import { configureStore, ConfigureStore } from "@reduxjs/toolkit";
-     import tableslice from "./table-slice";
-    const store=configureStore({
-        reducer:{ table:tableslice.reducer}
-    });
-
-    export default store;
