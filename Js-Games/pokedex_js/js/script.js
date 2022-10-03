4f3a21b8de092aec48037f5c719e77a47e2d1268
marilia-borgo <marilia.bm01@gmail.com>
2022-10-03 09:26:12 -0300

    adding comments in the code

--- Js-Games/pokedex_js/js/script.js
+++ Js-Games/pokedex_js/js/script.js
@@ -1,3 +1,4 @@
+//initial variables
 const PokemonName= document.querySelector('.pokemon_name');
 const PokemonNumber= document.querySelector('.pokemon_number');
 const PokemonImage= document.querySelector('.pokemon_image');
@@ -7,7 +8,7 @@ const buttonPrev = document.querySelector('.btn-prev');
 const buttonNext = document.querySelector('.btn-next');
 
 let searchPokemon=1;
-
+//bring the data from the pokeApi
 const fetchPokemon = async (pokemon) => {
     const APIresponse =  await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
     if (APIresponse.status ===200){
@@ -16,12 +17,12 @@ const fetchPokemon = async (pokemon) => {
 }
 
 const renderPokemon = async (pokemon)=>{
-
+//waiting for the response of the api
     PokemonName.innerHTML = 'carregando...';
     PokemonNumber.innerHTML = '';
 
     const data = await fetchPokemon(pokemon);
-
+//select the info that we want from the api
     if(data){
     PokemonName.innerHTML = data.name;
     PokemonNumber.innerHTML = data.id;
@@ -33,16 +34,16 @@ const renderPokemon = async (pokemon)=>{
         PokemonNumber='';
     }
 }
-
+//search the name of the pokemon from the search bar
 form.addEventListener('submit',(event) =>{
     event.preventDefault();
     renderPokemon(input.value.toLowerCase());
 });
-
+//button previous pokemon
 buttonPrev.addEventListener('click',(event) =>{
    alert('prev clicked')
 });
-
+//button next pokemon
 buttonNext.addEventListener('click',(event) =>{
     alert('next clicked')
  });
