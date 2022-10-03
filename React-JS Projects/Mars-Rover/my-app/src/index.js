a00a3d6e0fa60505f9fe5f332f24261f7143cb02
Umesh <umeshchavda05@gmail.com>
2022-10-04 01:18:15 +0530

    Adds Mars Rover Project

---
+++ React-JS Projects/Mars-Rover/my-app/src/index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import Page_2 from './Page2';
+
+
+
+
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    
+    <App />
+    <Page_2 />
+    
+    
+  </React.StrictMode>
+);
+
+
