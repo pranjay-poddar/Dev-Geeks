8254bdfe7eaaa21b5d104c2caf19919dbba80f74
Utkarsh Saxena <utkarshsaxena900@gmail.com>
2022-10-05 08:46:10 +0530

    Inserted AdBlocker Detection in Js projects folder

---
+++ Js-Projects/AdBlocker/script.js
@@ -0,0 +1,31 @@
+function adDetector()
+{
+    setTimeout(function()
+    {
+        document.body.innerHTML += `<div class="adsbygoogle" id="test-ad"></div>`;
+        const testAd = document.getElementById('test-ad');
+        const testAdStyle = getComputedStyle(testAd);
+    
+
+        if(testAdStyle.display === "none")
+        {
+            const banner = document.createElement('div');
+            banner.innerHTML= `<h2> Ad Blocker detected</h2>
+            <p> Using Advertising on this site</p>`
+             
+
+            banner.setAttribute('class','alert');
+            banner.style = "background: black; color: yellow"; 
+            document.body.prepend(banner);
+           
+        }
+           
+    
+        else
+          console.log("No Ad Blocker detected");
+    
+    },1000)
+
+}
+
+adDetector()
