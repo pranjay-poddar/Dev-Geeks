cffaeb0436d70d3e7b580ef10602e793e3906f5e
Faizan Choudhary <faizan2000461@st.jmi.ac.in>
2022-10-09 02:02:33 +0530

    added music_player in flutter projects

---
+++ Flutter Projects/music_player/android/app/src/main/kotlin/com/example/music_player/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.faizan.music_player
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ Flutter Projects/music_player/lib/models/Sounds.dart
@@ -0,0 +1,29 @@
+// List of imageUrls and soundUrls for SoundPlayer
+const List sounds = [
+  {
+    "id": 0,
+    "imageUrl": "https://www.99images.com/download-image/869224/2880x2560",
+    "title": "White Noise - Ocean",
+    "artist": "Nicole Johnson",
+    "soundUrl":
+    "https://firebasestorage.googleapis.com/v0/b/music-player-9b54b.appspot.com/o/white-noise.mp3?alt=media&token=a9c7aea6-358c-42e3-865b-d128196402b0"
+  },
+  {
+    "id": 1,
+    "imageUrl":
+    "https://music2relax.com/wp-content/uploads/2018/05/rsz_theraputic_binural_beats.jpg",
+    "title": "Binaural Beats",
+    "artist": "NaturesEye - Pixabay",
+    "soundUrl":
+    "https://firebasestorage.googleapis.com/v0/b/music-player-9b54b.appspot.com/o/binaural-beats.mp3?alt=media&token=7f5e4ac5-bd4a-4dcf-90f6-07bde3399612"
+  },
+  {
+    "id": 2,
+    "imageUrl":
+    "https://iasbaba.com/wp-content/uploads/2022/01/Mindfulness.png",
+    "title": "Mindfulness Aesthetic",
+    "artist": "NaturesEye - Pixabay",
+    "soundUrl":
+    "https://firebasestorage.googleapis.com/v0/b/music-player-9b54b.appspot.com/o/mindfulness-meditation.mp3?alt=media&token=aedc06a7-aab4-4555-91e8-b9c93e88e62a"
+  }
+];
