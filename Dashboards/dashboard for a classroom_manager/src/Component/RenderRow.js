a7ceb4381719a2dfd535cb671937468db2f534d9
ayushkedia05 <kediaayush26@gmail.com>
2022-10-09 10:37:11 +0530

    classroom dashboard

---
+++ Dashboards/dashboard for a classroom_manager/src/Component/RenderRow.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Table from './Table/Table'
+import "./RenderRow.css"
+
+function RenderRow() {
+  return (
+     <div className='renderer'>
+         <Table></Table>
+     </div>
+  
+   
+  )
+}
+
+export default RenderRow
