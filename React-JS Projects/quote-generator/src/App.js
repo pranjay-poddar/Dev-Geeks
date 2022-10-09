e27d07e64a5649581b48fe25e96e044020a2c22c
aditi10701 <aditiprasad4@gmail.com>
2022-10-09 14:38:05 +0530

    Quote generator

---
+++ React-JS Projects/quote-generator/src/App.js
@@ -0,0 +1,38 @@
+import React from 'react'
+import './App.css';
+import axios from 'axios'
+
+class App extends React.Component{
+  state = { advice: ''};
+    
+    componentDidMount(){
+     this.fetchAdvice();
+    }
+   fetchAdvice = () => {
+     axios.get('https://api.adviceslip.com/advice')
+     .then((response) => {
+       const {advice }= response.data.slip;
+       this.setState({advice});
+     })
+     .catch((error) => {
+       console.log(error);
+     });
+
+   }
+  
+  render(){
+    const {advice} = this.state;
+    return (
+      <div className="app">
+        <div className="card">
+          <h1 className="heading">{advice}</h1>
+          <button className="button" onClick={this.fetchAdvice}>
+            <span>GIVE ME ADVICE!</span>
+          </button>
+        </div>
+
+      </div>
+    );
+  }
+}                                             
+export default App;
