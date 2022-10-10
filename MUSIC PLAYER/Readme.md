76948aa8f26c0ba7fd6b544d384228e799e953a8
SubhrajyotiChak <87280247+SubhrajyotiChak@users.noreply.github.com>
2022-10-10 16:44:08 +0530

    Delete MUSIC PLAYER directory

--- MUSIC PLAYER/Readme.md
+++
@@ -1,3 +0,0 @@
-MUSIC PLAYER 
-
-![Screenshot 2022-10-10 164117](https://user-images.githubusercontent.com/87280247/194853468-f44112ea-1987-43b0-9d94-356f408bab01.png)
--- MUSIC PLAYER/script.js
+++
@@ -1,8 +0,0 @@
-let recent_volume= document.querySelector('#volume');
-let volume_show = document.querySelector('#volume_show');
-
-// change volume
-function volume_change(){
-	volume_show.innerHTML = recent_volume.value;
-	track.volume = recent_volume.value / 100;
-}
