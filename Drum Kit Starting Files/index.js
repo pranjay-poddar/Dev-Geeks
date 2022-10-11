e7fd221976e30977fed103ef2e7d01ba373ceb32
Ritu-2001 <100128956+Ritu-2001@users.noreply.github.com>
2022-10-11 15:13:59 +0530

    Add a mini Project -Drum kit using Sound Effect

---
+++ Drum Kit Starting Files/index.js
@@ -0,0 +1,50 @@
+var numberofDrumButtons=document.querySelectorAll(".drum").length;
+for (i=0;i<numberofDrumButtons;i++){
+
+
+
+
+document.querySelectorAll(".drum")[i].addEventListener("click",function(){
+var buttonInnerHTML=this.innerHTML;
+
+switch (buttonInnerHTML) {
+  case "w":
+  var tom1=new Audio("sounds/tom-1.mp3");
+  tom1.play();
+    break;
+
+    case"a":
+    var tom2=new Audio("sounds/tom-2.mp3");
+    tom2.play();
+    break;
+
+    case"s":
+    var tom3=new Audio("sounds/tom-3.mp3");
+    tom3.play();
+    break;
+    case"d":
+    var tom4=new Audio("sounds/tom-4.mp3");
+    tom4.play();
+    break;
+    case"j":
+    var snare=new Audio("sounds/snare.mp3");
+    snare.play();
+    break;
+    case"k":
+    var crash=new Audio("sounds/crash.mp3");
+    crash.play();
+    break;
+    case"l":
+    var kick=new Audio("sounds/kick-bass.mp3");
+    kick.play();
+    break;
+
+
+    default:console.log(buttonInnerHTML);
+
+}
+
+
+});
+
+}
