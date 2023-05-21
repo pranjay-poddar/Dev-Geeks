cc34244d2bbb3335ac6f050ba8d1527c57803145
Abhishek Kumar <94506000+AbhishekCS3459@users.noreply.github.com>
2023-05-21 21:15:27 +0000

    Added React Animated Weather App

---
+++ React-JS Projects/Live_Weather/src/App.js
@@ -0,0 +1,15 @@
+import React, { useState } from "react";
+import CurrentLocation from "./currentLocation";
+import "./App.css";
+
+function App() {
+  return (
+    <React.Fragment>
+      <div className="container">
+        <CurrentLocation />
+      </div>
+    </React.Fragment>
+  );
+}
+
+export default App;
---
+++ React-JS Projects/Live_Weather/src/ButtonA.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function ButtonA() {
+  return (
+    <div>
+      <button className='btnplay'>
+
+      </button>
+      <div className="display">
+        
+      </div>
+    </div>
+  )
+}
