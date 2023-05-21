a2024d1009ec62be4bf405bd1e9a68e99df836a1
Abhishek Kumar <94506000+AbhishekCS3459@users.noreply.github.com>
2023-05-21 21:15:56 +0000

    Added React Animated_Weather App

--- React-JS Projects/Live_Weather/src/apiKeys.js
+++
@@ -1,5 +0,0 @@
-// Visit https://api.openweathermap.org & then signup to get our API keys for free
-module.exports = {
-  key: "Enter Your API Key Here",
-  base: "https://api.openweathermap.org/data/2.5/",
-};
