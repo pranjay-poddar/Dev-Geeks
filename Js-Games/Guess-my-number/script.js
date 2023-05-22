5fc017a32140f94f6f2321333effe25bbe33f8d4
ayan878 <ayanraza197@gmail.com>
2023-05-22 11:29:55 +0530

    Celebrating Features

--- Js-Games/Guess-my-number/script.js
+++ Js-Games/Guess-my-number/script.js
@@ -51,6 +51,9 @@ document.querySelector(".check").addEventListener("click", function () {
     document.querySelector("body").style.backgroundColor = "#60b347";
     document.querySelector(".number").style.width = "30rem";
 
+    const jsConfetti = new JSConfetti();
+    jsConfetti.addConfetti();
+
     if (score > highscore) {
       highscore = score;
       document.querySelector(".highscore").textContent = highscore;
