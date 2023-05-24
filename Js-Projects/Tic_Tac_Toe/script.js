6c4c549e99bfbf5d6eec3a493150bbf2efbb0818
AyishikD <99983449+AyishikD@users.noreply.github.com>
2023-05-24 23:52:37 +0530

    Added play vs Ai

--- Js-Projects/Tic_Tac_Toe/script.js
+++ Js-Projects/Tic_Tac_Toe/script.js
@@ -1,9 +1,17 @@
 let btnRef = document.querySelectorAll(".button-option");
 let popupRef = document.querySelector(".popup");
-let newgameBtn = document.getElementById("new-game");
+let newGameBtn = document.getElementById("new-game");
 let restartBtn = document.getElementById("restart");
-let msgRef = document.getElementById("message");
-let winningPattern = [
+let messageRef = document.getElementById("message");
+let gameBoard = document.getElementById("game-board");
+let optionButtons = document.querySelectorAll(".option-btn");
+
+let xTurn = true;
+let count = 0;
+let gameActive = false;
+let isAgainstAI = false;
+
+const winningPattern = [
   [0, 1, 2],
   [0, 3, 6],
   [2, 5, 8],
@@ -13,69 +21,185 @@ let winningPattern = [
   [0, 4, 8],
   [2, 4, 6],
 ];
-let xTurn = true;
-let count = 0;
+
 const disableButtons = () => {
   btnRef.forEach((element) => (element.disabled = true));
-  popupRef.classList.remove("hide");
 };
+
 const enableButtons = () => {
   btnRef.forEach((element) => {
     element.innerText = "";
     element.disabled = false;
   });
+};
+
+const showPopup = (text) => {
+  messageRef.innerHTML = text;
+  popupRef.classList.remove("hide");
+};
+
+const hidePopup = () => {
   popupRef.classList.add("hide");
 };
-const winFunction = (letter) => {
-  disableButtons();
-  if (letter == "X") {
-    msgRef.innerHTML = "&#x1F389; <br> 'X' Wins";
-  } else {
-    msgRef.innerHTML = "&#x1F389; <br> 'O' Wins";
+
+const checkWin = (player) => {
+  for (let pattern of winningPattern) {
+    if (
+      btnRef[pattern[0]].innerText === player &&
+      btnRef[pattern[1]].innerText === player &&
+      btnRef[pattern[2]].innerText === player
+    ) {
+      return true;
+    }
   }
+  return false;
 };
-const drawFunction = () => {
-  disableButtons();
-  msgRef.innerHTML = "&#x1F60E; <br> It's a Draw";
+
+const checkDraw = () => {
+  return Array.from(btnRef).every((button) => button.innerText !== "");
 };
-newgameBtn.addEventListener("click", () => {
-  count = 0;
-  enableButtons();
-});
-restartBtn.addEventListener("click", () => {
-  count = 0;
-  enableButtons();
-});
-const winChecker = () => {
-  for (let i of winningPattern) {
-    let [element1, element2, element3] = [
-      btnRef[i[0]].innerText,
-      btnRef[i[1]].innerText,
-      btnRef[i[2]].innerText,
-    ];
-    if (element1 != "" && (element2 != "") & (element3 != "")) {
-      if (element1 == element2 && element2 == element3) {
-        winFunction(element1);
+
+const makeAIMove = () => {
+    // Check if AI can win in the next move
+    for (let pattern of winningPattern) {
+      const [a, b, c] = pattern;
+      if (
+        btnRef[a].innerText === "O" &&
+        btnRef[b].innerText === "O" &&
+        btnRef[c].innerText === ""
+      ) {
+        btnRef[c].innerText = "O";
+        return;
+      }
+      if (
+        btnRef[a].innerText === "O" &&
+        btnRef[c].innerText === "O" &&
+        btnRef[b].innerText === ""
+      ) {
+        btnRef[b].innerText = "O";
+        return;
+      }
+      if (
+        btnRef[b].innerText === "O" &&
+        btnRef[c].innerText === "O" &&
+        btnRef[a].innerText === ""
+      ) {
+        btnRef[a].innerText = "O";
+        return;
       }
     }
-  }
+  
+    // Check if the player can win in the next move and block them
+    for (let pattern of winningPattern) {
+      const [a, b, c] = pattern;
+      if (
+        btnRef[a].innerText === "X" &&
+        btnRef[b].innerText === "X" &&
+        btnRef[c].innerText === ""
+      ) {
+        btnRef[c].innerText = "O";
+        return;
+      }
+      if (
+        btnRef[a].innerText === "X" &&
+        btnRef[c].innerText === "X" &&
+        btnRef[b].innerText === ""
+      ) {
+        btnRef[b].innerText = "O";
+        return;
+      }
+      if (
+        btnRef[b].innerText === "X" &&
+        btnRef[c].innerText === "X" &&
+        btnRef[a].innerText === ""
+      ) {
+        btnRef[a].innerText = "O";
+        return;
+      }
+    }
+  
+    // If no winning or blocking move is available, make a random move
+    const emptyIndexes = [];
+    btnRef.forEach((button, index) => {
+      if (button.innerText === "") {
+        emptyIndexes.push(index);
+      }
+    });
+  
+    const randomIndex = emptyIndexes[Math.floor(Math.random() * emptyIndexes.length)];
+    btnRef[randomIndex].innerText = "O";
+  };
+  
+
+const handleClick = (event) => {
+    const button = event.target;
+    if (button.innerText === "" && gameActive) {
+      button.innerText = "X";
+      count++;
+  
+      if (checkWin(button.innerText)) {
+        disableButtons();
+        showPopup(`Player ${button.innerText} wins!`);
+      } else if (checkDraw()) {
+        disableButtons();
+        showPopup("It's a draw!");
+      } else {
+        xTurn = !xTurn;
+  
+        if (isAgainstAI && gameActive && button.innerText !== "O") {
+          makeAIMove();
+          count++;
+  
+          if (checkWin("O")) {
+            disableButtons();
+            showPopup("AI wins!");
+          } else if (checkDraw()) {
+            disableButtons();
+            showPopup("It's a draw!");
+          }
+        }
+      }
+    }
+  };
+  
+  
+  
+  
+const startGameAI = () => {
+  isAgainstAI = true;
+  gameActive = true;
+  hidePopup();
+  enableButtons();
 };
-btnRef.forEach((element) => {
-  element.addEventListener("click", () => {
-    if (xTurn) {
-      xTurn = false;
-      element.innerText = "X";
-      element.disabled = true;
-    } else {
-      xTurn = true;
-      element.innerText = "O";
-      element.disabled = true;
+
+const startGameUser = () => {
+  isAgainstAI = false;
+  gameActive = true;
+  hidePopup();
+  enableButtons();
+};
+
+const handleRestart = () => {
+  count = 0;
+  xTurn = true;
+  enableButtons();
+  hidePopup();
+  btnRef.forEach((button) => (button.innerText = ""));
+  gameActive = true;
+};
+
+optionButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    document.querySelector(".options").classList.add("hide");
+    gameBoard.classList.remove("hide");
+    if (button.id === "btn-ai") {
+      startGameAI();
+    } else if (button.id === "btn-user") {
+      startGameUser();
     }
-    count += 1;
-    if (count == 9) {
-      drawFunction();
-    }
-    winChecker();
   });
 });
-window.onload = enableButtons;
+
+btnRef.forEach((button) => button.addEventListener("click", handleClick));
+newGameBtn.addEventListener("click", handleRestart);
+restartBtn.addEventListener("click", handleRestart);
