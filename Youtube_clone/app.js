61cb81e04707a06b8172a8a2bb7d92b12d2e84f1
Enhancer18 <102911149+Enhancer18@users.noreply.github.com>
2023-06-03 15:15:24 +0530

    Update app.js

--- Youtube_clone/app.js
+++ Youtube_clone/app.js
@@ -1,6 +1,6 @@
 const videoCardContainer = document.querySelector('.video-container');
 
-let api_key = "AIzaSyCkjGc7yNuux1OSxbzXAkYSl_PNXyMsTIs";
+let api_key = "YOUR_API_KEY";
 let video_http = "https://www.googleapis.com/youtube/v3/videos?";
 let channel_http = "https://www.googleapis.com/youtube/v3/channels?";
 
