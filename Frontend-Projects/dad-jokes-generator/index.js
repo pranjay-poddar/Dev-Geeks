c8cfe96db3e8241b3cb940444d5aef48e1a6d059
Sagar Rana <sagarrana.pvt@gmail.com>
2023-06-10 21:22:43 +0530

    [GSSoC'23] Added a dad jokes generator to frontend projects (#1952)
    * added dad jokes generator
    
    * Update README.md
    
    * Update README.md
    
    * Create README.md
    
    * Update index.html
    
    * Update index.js
    
    * Update README.md
    
    * Update README.md

---
+++ Frontend-Projects/dad-jokes-generator/index.js
@@ -0,0 +1,38 @@
+const btnEl = document.getElementById("btn");
+const jokeEl = document.getElementById("joke");
+// GET THE API THROUGH API KEY
+const apiKey = "4kqGcJx8uDXo3XIskcbzokAz7rN8nWJs3PL9Mcll";
+
+const options = {
+  method: "GET",
+  headers: {
+    "X-Api-Key": apiKey,
+  },
+};
+
+const apiURL = "https://api.api-ninjas.com/v1/dadjokes?limit=1";
+
+// TRY-CATCH CODE FOR ERROR HANDLING
+
+async function getJoke() {
+  try {
+    jokeEl.innerText = "Updating...";
+    btnEl.disabled = true;
+    btnEl.innerText = "Loading...";
+    const response = await fetch(apiURL, options);
+    const data = await response.json();
+
+    btnEl.disabled = false;
+    btnEl.innerText = "Tell me a joke";
+
+    jokeEl.innerText = data[0].joke;
+  } catch (error) {
+    jokeEl.innerText = "An error happened, try again later";
+    btnEl.disabled = false;
+    btnEl.innerText = "Tell me a joke";
+    console.log(error);
+  }
+}
+
+btnEl.addEventListener("click", getJoke);
+
