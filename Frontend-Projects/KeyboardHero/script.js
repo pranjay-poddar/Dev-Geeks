2babe731ef8e01b65b783172578a7c9f672b4703
MisterMickey <singodiyapeechuparashubham@gmail.com>
2023-06-21 08:09:29 +0530

    add keyboard hero

---
+++ Frontend-Projects/KeyboardHero/script.js
@@ -0,0 +1,46 @@
+const keys = [..."ABCDEFGHIJKLMNOPQRSTUVWXYZ"];
+
+const timestamps = [];
+
+timestamps.unshift(getTimestamp());
+
+function getRandomNumber(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function getRandomKey() {
+  return keys[getRandomNumber(0, keys.length-1)]
+}
+
+function targetRandomKey() {
+  const key = document.getElementById(getRandomKey());
+  key.classList.add("selected");
+  let start = Date.now()
+}
+
+function getTimestamp() {
+  return Math.floor(Date.now() / 1000)
+}
+
+document.addEventListener("keyup", event => {
+  const keyPressed = String.fromCharCode(event.keyCode);
+  const keyElement = document.getElementById(keyPressed);
+  const highlightedKey = document.querySelector(".selected");
+  
+  keyElement.classList.add("hit")
+  keyElement.addEventListener('animationend', () => {
+    keyElement.classList.remove("hit")
+  })
+  
+  if (keyPressed === highlightedKey.innerHTML) {
+    timestamps.unshift(getTimestamp());
+    const elapsedTime = timestamps[0] - timestamps[1];
+    console.log(`Character per minute ${60/elapsedTime}`)
+    highlightedKey.classList.remove("selected");
+    targetRandomKey();
+  } 
+})
+
+targetRandomKey();
--- README.md
+++ README.md
@@ -760,9 +760,10 @@ Happy Coding! -->
 <td align="center"> <a href="https://github.com/pranjay-poddar/Dev-Geeks/tree/main/Frontend-Projects/VegetableMarketplace"> VegetableMarketplace </a></td>
 <td align="center"> 250.</td>
 <td align="center"> <a href="https://github.com/pranjay-poddar/Dev-Geeks/tree/main/Frontend-Projects/QrComponent"> QrComponent </a></td>
+<td align="center"> 251.</td>
+<td align="center"> <a href="https://github.com/pranjay-poddar/Dev-Geeks/tree/main/Frontend-Projects/KeyboardHero"> KeyboardHero </a></td>
 </tr>
 </table>
-
 
 
 ## How to contribute:
