a0bfadb0a9c09a8e3dbfc926442573b3eb0fe12b
Bharti Vilas Nagpure <104631882+BhartiNagpure@users.noreply.github.com>
2023-06-23 12:10:20 +0530

    data updated

--- notification/src/Data.js
+++ notification/src/Data.js
@@ -7,13 +7,6 @@ export const posts =[
         userImg: "https://images.pexels.com/photos/1557843/pexels-photo-1557843.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
         postImg:"https://images.pexels.com/photos/904117/pexels-photo-904117.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
     },
-    // {
-    //     id:2,
-    //     username:"Shital",
-    //     fullname :"Shital Nagpure",
-    //     userImg: "https://images.pexels.com/photos/1116380/pexels-photo-1116380.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-    //     postImg:"https://images.pexels.com/photos/1140854/pexels-photo-1140854.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-    // },
     {
         id:3,
         username:"nayan",
