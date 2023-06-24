48c737d305c5453e03a2f53737d1a2ce8bc31a9b
iamvittaln <62231556+iamvittaln@users.noreply.github.com>
2023-06-24 23:58:42 +0530

    Signup and login completed

--- PHP Projects/User_Signup_Login_with_database/signup-success.php
+++ PHP Projects/User_Signup_Login_with_database/signup-success.php
@@ -1,22 +1,24 @@
+<?php
+    if(!(isset($_GET["update"])))
+    {
+        die();
+    }
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Index page</title>
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <title>Signup Success</title>
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
     <!-- Bootstrap CSS -->
 </head>
 <body class="bg-dark">
-    <div class="container">
-        <p class="display-1 text-center text-success">Welcome to our website!</p>
-        <!-- Links to redirect to signup and login pages -->
-        <div class="container d-flex justify-content-center border border-5">
-            <p class="display-4 p-5"><a href="signup.php" class="text-decoration-none">Signup</a></p>
-            <p class="display-4 p-5"><a href="login.php" class="text-decoration-none">Login</a></p>
-        </div>
+    <div class="container d-flex flex-column justify-content-center align-items-center mt-5 border border-5">
+        <p class="display-3 text-success">Signed up successfully!</p>
+        <p class="display-5 text-secondary">You can <a href="login.php" class="text-decoration-none">login here</a></p>
     </div>
     <!-- Bootstrap JS -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"
