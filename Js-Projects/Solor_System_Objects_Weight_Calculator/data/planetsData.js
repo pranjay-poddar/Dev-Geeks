5d9d102d8260102df9bb0f2a70957e9db0da70e4
Abdul Khalid <mezmerizingbeats2004@gmail.com>
2023-07-02 01:29:58 +0530

    add: solar system objects weights calculator (#2836)

---
+++ Js-Projects/Solor_System_Objects_Weight_Calculator/data/planetsData.js
@@ -0,0 +1,11 @@
+// Planet data with mass, radius, and surface gravity
+const planetData = {
+  mercury: { mass: 3.30e23, radius: 2.44e6, surfaceGravity: 3.7 },
+  venus: { mass: 4.87e24, radius: 6.05e6, surfaceGravity: 8.87 },
+  earth: { mass: 5.97e24, radius: 6.37e6, surfaceGravity: 9.81 },
+  mars: { mass: 6.39e23, radius: 3.39e6, surfaceGravity: 3.71 },
+  jupiter: { mass: 1.90e27, radius: 6.99e7, surfaceGravity: 24.79 },
+  saturn: { mass: 5.68e26, radius: 5.82e7, surfaceGravity: 10.44 },
+  uranus: { mass: 8.68e25, radius: 2.54e7, surfaceGravity: 8.69 },
+  neptune: { mass: 1.02e26, radius: 2.46e7, surfaceGravity: 11.15 }
+};
