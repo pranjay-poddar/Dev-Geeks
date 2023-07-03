54f08246ff1ab512df61dbc7576402dfa51f60f8
Bharti Vilas Nagpure <104631882+BhartiNagpure@users.noreply.github.com>
2023-07-03 11:47:47 +0530

    realtime notification app

---
+++ React-JS Projects/Real_Time_Notification/notification/src/Data.js
@@ -0,0 +1,18 @@
+
+export const posts =[
+    {
+        id:1,
+        username:"bharti",
+        fullname :"Bharti Nagpure",
+        userImg: "https://images.pexels.com/photos/1557843/pexels-photo-1557843.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+        postImg:"https://images.pexels.com/photos/904117/pexels-photo-904117.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+    },
+    {
+        id:3,
+        username:"nayan",
+        fullname :"Nayan Nagpure",
+        userImg: "https://images.pexels.com/photos/1105058/pexels-photo-1105058.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+        postImg:"https://images.pexels.com/photos/1553783/pexels-photo-1553783.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+    }
+
+]
