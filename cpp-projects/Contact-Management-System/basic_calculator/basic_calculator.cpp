46f9e9f14efdaac6f998732b8b63966f6e8cf89f
Nikita06211 <120494269+Nikita06211@users.noreply.github.com>
2023-07-18 16:34:47 +0530

    Update basic_calculator.cpp

--- cpp-projects/Contact-Management-System/basic_calculator/basic_calculator.cpp
+++ cpp-projects/Contact-Management-System/basic_calculator/basic_calculator.cpp
@@ -8,7 +8,7 @@ int main() {
     double num1, num2, result;
 
     // Get operator symbol from the user
-    cout << "Enter operator\n1. + for addition\n2. - for subtraction\n3. * for multiplication\n4. / for division\n5. ^ for calculating power\n6. S for sine\n7. C for cosine\n8. T for tan "<<endl;
+    cout << "Enter operator\n1. + for addition\n2. - for subtraction\n3. * for multiplication\n4. / for division\n5. ^ for calculating power\n6. S for sine\n7. C for cosine\n8. T for tan\n9. e for exponential\n10. l for log\n11. r for rounding off\n12. f for factorial\n13. p for percentage "<<endl;
     cin >> operatorSymbol;
 
     // Perform the operation based on the operator symbol
@@ -57,6 +57,36 @@ int main() {
             cout << "Enter angle in Radians"<<endl;
             cin>>num1;
             result = tan(num1);
+            break;
+        case 'e':
+            cout<<"Enter number"<<endl;
+            cin>>num1;
+            result=exp(num1);
+            break;
+        case 'l':
+            cout<<"Enter number"<<endl;
+            cin>>num1;
+            result=log(num1);
+            break;
+        case 'r':
+            cout<<"Enter number"<<endl;
+            cin>>num1;
+            result=round(num1);
+            break;
+        case 'f':
+            cout<<"Enter number"<<endl;
+            cin>>num1;
+            for(int i=0;i<=num1;i++){
+                fact=fact*i;
+            }
+            result=fact;
+            break;
+        case 'p':
+            cout<<"Enter marks obtained"<<endl;
+            cin>>num1;
+            cout<<"Enter total marks"<<endl;
+            cin>>num2;
+            result=100*((num1)/(num2));
             break;
         default:
             cout << "Error: Invalid operator." << endl;
