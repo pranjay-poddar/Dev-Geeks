f69bf4bbc4ca24ba562045f93f0e698fe28fa053
smty2018 <74114936+smty2018@users.noreply.github.com>
2023-07-31 20:40:04 +0530

    Add files via upload

---
+++ Js-Projects/image slider/script.js
@@ -0,0 +1,20 @@
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusDivs(n) {
+  showSlides(slideIndex += n);
+}
+
+function showSlides(n) {
+  let slides = document.getElementsByClassName("mySlides");
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slides[slideIndex - 1].style.display = "block";
+}
