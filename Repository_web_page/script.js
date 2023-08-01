3e6f9481584bfad944bccf06b80be56bbc9271c7
Sumit Kumar Rai <100675296+sumitkr2000@users.noreply.github.com>
2023-08-01 13:05:09 +0530

    improved the ui fo the website

--- Repository_web_page/script.js
+++ Repository_web_page/script.js
@@ -1,20 +1,23 @@
 let calcScrollValue = () => {
-    let scrollProg = document.getElementById("progress");
-    let pos = document.documentElement.scrollTop;
-    let calcHeight =
-      document.documentElement.scrollHeight -
-      document.documentElement.clientHeight;
-    let scrollValue = Math.round((pos * 100) / calcHeight);
-    if (pos > 100) {
-      scrollProg.style.display = "grid";
-    } else {
-      scrollProg.style.display = "none";
-    }
-    scrollProg.addEventListener("click", () => {
-      document.documentElement.scrollTop = 0;
-    });
-    scrollProg.style.background = `conic-gradient(#1260CC ${scrollValue}%, #d7d7d7 ${scrollValue}%)`;
-  };
-  
-  window.onscroll = calcScrollValue;
-  window.onload = calcScrollValue;
+  let scrollProg = document.getElementById("progress");
+  let pos = document.documentElement.scrollTop;
+  let calcHeight =
+    document.documentElement.scrollHeight -
+    document.documentElement.clientHeight;
+  let scrollValue = Math.round((pos * 100) / calcHeight);
+  if (pos > 100) {
+    scrollProg.style.display = "grid";
+  } else {
+    scrollProg.style.display = "none";
+  }
+  scrollProg.addEventListener("click", () => {
+    document.documentElement.scrollTop = 0;
+  });
+  scrollProg.style.background = `conic-gradient(#D367CB ${scrollValue}%, #d7d7d7 ${scrollValue}%)`;
+};
+
+window.onscroll = calcScrollValue;
+window.onload = calcScrollValue;
+
+var copy = document.querySelector(".button-slide").cloneNode(true);
+document.querySelector(".experience").appendChild(copy);
