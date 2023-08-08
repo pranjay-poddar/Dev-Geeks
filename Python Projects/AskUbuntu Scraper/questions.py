d1c670b5361edfef1e1c8eb6f36ca47ac67da783
Syed Md Mihan Chistie <42113027+Mihan786Chistie@users.noreply.github.com>
2023-08-08 05:45:39 +0000

    added getActiveQuestions() function

--- Python Projects/AskUbuntu Scraper/questions.py
+++ Python Projects/AskUbuntu Scraper/questions.py
@@ -73,3 +73,43 @@ class AskUbuntu:
 
             ejson = json.dumps(error_message)
             return ejson
+
+    def getActiveQuestions(self):
+        
+        url = "https://askubuntu.com/questions/tagged/" + self.topic + "?tab=Active"
+        try:
+            res = requests.get(url)
+            soup = BeautifulSoup(res.text, "html.parser")
+
+            questions_data = {"questions": []}
+
+            questions = soup.select(".s-post-summary")
+            for que in questions:
+                title = que.select_one(".s-link").getText()
+                stats = que.select(".s-post-summary--stats-item-number")
+                vote = stats[0].getText()
+                ans = stats[1].getText()
+                views = stats[2].getText()
+                desc = (
+                    que.select_one(".s-post-summary--content-excerpt")
+                    .getText()
+                    .strip()
+                    .encode("ascii", "ignore")
+                    .decode()
+                )
+                questions_data["questions"].append(
+                    {
+                        "question": title,
+                        "views": views,
+                        "vote_count": vote,
+                        "answer_count": ans,
+                        "description": desc,
+                    }
+                )
+            json_data = json.dumps(questions_data)
+            return json_data
+        except:
+            error_message = {"message": "No questions related to the topic found"}
+
+            ejson = json.dumps(error_message)
+            return ejson
