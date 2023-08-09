da4bb188ef3a3fb243af265a85fe7dc9c55f6499
SHREYAS SUKHADEVE <96542494+Shreyas-SAS@users.noreply.github.com>
2023-08-10 00:20:00 +0530

    Comment + Add challenge

--- Frontend-Projects/Image_based_Password_Strength/script.js
+++ Frontend-Projects/Image_based_Password_Strength/script.js
@@ -4,6 +4,9 @@ const background = document.getElementById('background')
 password.addEventListener('input', (e) => {
   const input = e.target.value
   const length = input.length
-  const blurValue = 20 - length * 2
+  const blurValue = 20 - length * 2 // blur is only defined on length for simplicity. 
+  // I wish to add a challenge to it. Make it such that symbols contribute 2x clarity vs other characters.
+
+  // applying blur to background
   background.style.filter = `blur(${blurValue}px)`
 })
